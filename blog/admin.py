4622b0580dfa1a9e1369edaf4f8ea38096ad2962
Logan <Lhugli6386@hotmail.com>
2019-02-13 12:44:58 -0500

    Added bootstrap

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+# Adding Blogs to admin portal
+from .models import Blog 
 
-# Register your models here.
+admin.site.register(Blog)
--- jobs/admin.py
+++ jobs/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 # Adding Jobs to admin portal
 from .models import Job 
 
-
 admin.site.register(Job)
 
